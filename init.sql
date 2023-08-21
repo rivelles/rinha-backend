e511aa19de56c3a1126df3bdfa92b8863055fff3
Lucas Rivelles <lucas.rivelles@glovoapp.com>
2023-08-21 22:41:10 +0200

    fix: Fix init.sql

--- init.sql
+++ init.sql
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS pessoas (
 CREATE UNIQUE INDEX IF NOT EXISTS external_idx ON PESSOAS (external_id);
 
 ALTER TABLE pessoas ADD COLUMN ts tsvector
-    GENERATED ALWAYS AS (to_tsvector('portuguese', coalesce(apelido, '') || ' ' || coalesce(nome, '') || ' ' || coalesce(stacks))) STORED;
+    GENERATED ALWAYS AS (to_tsvector('portuguese', coalesce(apelido, '') || ' ' || coalesce(nome, '') || ' ' || coalesce(stacks, ''))) STORED;
 
 CREATE INDEX ts_idx ON pessoas USING gin(ts);
